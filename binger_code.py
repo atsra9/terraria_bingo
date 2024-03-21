0b23b89b785204c086aebdf3ed9fee9ceb0ddeef
EmeraldBlock <yygengjunior@gmail.com>
2024-03-20 20:34:01 -0500

    use library functions

--- binger_code.py
+++ binger_code.py
@@ -1,11 +1,9 @@
 import random
-import math
 import json
+from enum import Enum, auto
 
 # To-Do:
 # Randomize individual tasks
-
-bingerBoard = []
 
 easyTasks = ['1 of each grave (no golden variants)',
              '- Molotov Cocktails', # 30-60
@@ -91,45 +89,38 @@ insaneTasks = [ 'Flamarang',
                 'Sunfury'
                 ]
 
-numAllTasks = [len(tasks) for tasks in [easyTasks, medTasks, hardTasks, insaneTasks]]
+allTasks = [easyTasks, medTasks, hardTasks, insaneTasks]
+numAllTasks = [len(tasks) for tasks in allTasks]
+
+class Color(Enum):
+    MIDDLE = auto()
+    GREEN = auto()
+    YELLOW = auto()
+
+B = Color.MIDDLE
+G = Color.GREEN
+Y = Color.YELLOW
+colors = [
+    Y, Y, Y, Y, G,
+    Y, G, Y, Y, Y,
+    Y, Y, B, Y, Y,
+    Y, Y, Y, G, Y,
+    G, Y, Y, Y, Y,
+]
+
+weights = {
+    Color.MIDDLE: [0, 0, 0, 1],
+    Color.GREEN: [0, 2, 7, 1],
+    Color.YELLOW: [50, 40, 8, 2],
+}
+
+bingerBoard = []
 
 for i in range(25):
     rand = random.random()
-    if i == 12:                                                     # Middle Square, 100% Insane
-        insaneRand = math.floor(rand*len(insaneTasks))
-        name = insaneTasks[insaneRand]
-        insaneTasks.remove(insaneTasks[insaneRand])
-    elif (i == 4 or i == 6 or i == 18 or i == 20):                  # Green Squares (see sheet), 20% Medium, 70% Hard, 10% Insane
-        if random.randint(1, 5) == 1:                               # 20% Medium
-            medRand = math.floor(rand*len(medTasks))
-            name = medTasks[medRand]
-            medTasks.remove(medTasks[medRand])
-        elif random.randint(1, 8) != 8:                             # 87.5% of remaining 80% Hard (70%)
-            hardRand = math.floor(rand*len(hardTasks))
-            name = hardTasks[hardRand]
-            hardTasks.remove(hardTasks[hardRand])
-        else:                                                       # Remaining 10% Insane
-            insaneRand = math.floor(rand*len(insaneTasks))
-            name = insaneTasks[insaneRand]
-            insaneTasks.remove(insaneTasks[insaneRand])
-    else:                                                           # Yellow Squares (see sheet), 50% Easy, 40% Medium, 8% Hard, 2% Insane
-        if random.randint(1, 2) == 1:                               # 50% Easy
-            easyRand = math.floor(rand*len(easyTasks))
-            name = easyTasks[easyRand]
-            easyTasks.remove(easyTasks[easyRand])
-        elif random.randint(1, 5) != 5:                             # 80% of remaining 50% Medium (40%)
-            medRand = math.floor(rand*len(medTasks))
-            name = medTasks[medRand]
-            medTasks.remove(medTasks[medRand])
-        elif random.randint(1, 5) != 5:                             # 80% of remaining 10% Hard (8%)
-            hardRand = math.floor(rand*len(hardTasks))
-            name = hardTasks[hardRand]
-            hardTasks.remove(hardTasks[hardRand])
-        else:                                                       # Remaining 2% Insane
-            insaneRand = math.floor(rand*len(insaneTasks))
-            name = insaneTasks[insaneRand]
-            insaneTasks.remove(insaneTasks[insaneRand])
-    bingerBoard.append({'name': name})
+    tasks = random.choices(allTasks, weights[colors[i]])[0]
+    task = tasks.pop(random.randrange(len(tasks)))
+    bingerBoard.append({'name': task})
 
 print(json.dumps(bingerBoard, separators=(',', ':')))
 print(numAllTasks)
